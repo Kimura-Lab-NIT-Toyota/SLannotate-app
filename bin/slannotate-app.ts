5b512dadf25453dba05a79e733c21d2d99124be7
fuu38 <contact@fumiya.biz>
2023-05-11 15:37:58 +0900

    rename SLaApp to SLaFront

--- bin/slannotate-app.ts
+++ bin/slannotate-app.ts
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { SLannotateAppStack } from '../lib/slannotate-app-stack';
+import { SLannotateFrontStack } from '../lib/slannotate-front-stack';
 
 const app = new cdk.App();
-new SLannotateAppStack(app, 'SLannotateAppStack', {
+new SLannotateFrontStack(app, 'SLannotateFrontStack', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */

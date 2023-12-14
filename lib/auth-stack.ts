d9ea7af12c7a4da85708313b25f333bb73a3af59
fuu38 <contact@fumiya.biz>
2023-12-14 16:51:24 +0900

    deleted unused stack&delete emila requiration

--- lib/auth-stack.ts
+++ lib/auth-stack.ts
@@ -8,24 +8,15 @@ export class CognitoAuthStack extends Stack {
 
         const userPool = new aws_cognito.UserPool(this, 'SLannotateUserPool', {
             userPoolName: 'SLannotateUserPool',
-            selfSignUpEnabled: true,//ユーザーに自分で登録させるならT、IDPassをくばるならF
-            accountRecovery: aws_cognito.AccountRecovery.EMAIL_ONLY,
-            standardAttributes: {
-                email: {
-                    required: true,
-                    mutable: true,//true場合emailアドレスの変更が可能
-                },
-            },
-            autoVerify: { email: true },
-            signInAliases: { email: true, username: true },
-            removalPolicy: RemovalPolicy.DESTROY,//TODO:デプロイ時にユーザーをすべて削除。本番環境ではRETAINにしてください。
+            selfSignUpEnabled: false,//ユーザーに自分で登録させるならT、IDPassをくばるならF
+            signInAliases: {username: true },//TODO:デプロイ時にユーザーをすべて削除。本番環境ではRETAINにしてください。
         });
 
         userPool.addClient("SLannotateUserPoolClient", {
             userPoolClientName: "SLannotateUserPoolClient",
             generateSecret: false,
             oAuth: {
-                callbackUrls: ["https://d2guuix4ia1xx3.cloudfront.net/"],
+                callbackUrls: ["https://d2guuix4ia1xx3.cloudfront.net/"],//TODO:これ昔のやつ。消さなくて動いてるし無理に消す必要ないけど、いらないのがあるのきもい。
                 flows: {
                     implicitCodeGrant: true,
                     authorizationCodeGrant: true,
